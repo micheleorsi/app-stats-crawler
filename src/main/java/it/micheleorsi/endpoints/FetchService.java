b5fb51d2e75c07bd426e4e017c36ba1c20e565b8
Michele Orsi <morsi@map2app.com>
2013-11-22 02:09:02 +0100

    it works with jersey 1.17.1

---
+++ src/main/java/it/micheleorsi/endpoints/FetchService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package it.micheleorsi.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author micheleorsi
+ *
+ */
+@Path("/hello")
+public class FetchService {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "Hello world";
+	}
+
+}
